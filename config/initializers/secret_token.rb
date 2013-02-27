fc7347849cc3a497f52caa2e8e7d0a91c97f1528
ogdenit <mitch@ogdenit.com>
2013-02-27 05:48:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ogah::Application.config.secret_token = 'e27ac4dac6f394550f0425b40851cdeaa343d43da73395214507fb0d8ceced7de26e851f197787b085e73d87cf29110f9c98780be08dadfd739bcd0717b5f4aa'
